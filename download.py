b920aba90d434c880b2f2a650bc1ee7dbc867ed6
Thomas Kuoch <kuoch.thomas@gmail.com>
2024-11-13 17:41:34 +0100

    fix: add loguru

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
-# https://github.com/PaddlePaddle/PaddleOCR/issues/11530#issuecomment-2017651736
-
+from loguru import logger
 from paddleocr import PaddleOCR
 
 if __name__ == "__main__":
+    # https://github.com/PaddlePaddle/PaddleOCR/issues/11530#issuecomment-2017651736
+    logger.info("Downloading PaddleOCR files...")
     PaddleOCR(lang="fr", use_angle_cls=True, det_db_score_mode="slow")
